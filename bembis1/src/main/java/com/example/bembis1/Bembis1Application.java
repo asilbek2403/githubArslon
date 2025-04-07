6037ad76cf05e9e303b98aa518c28d97a97eacde
Asilbek <asilbeksafarov962@gmail.com>
2025-04-07 17:51:40 +0500

    first connect INT IDEA Java

---
+++ bembis1/src/main/java/com/example/bembis1/Bembis1Application.java
@@ -0,0 +1,13 @@
+package com.example.bembis1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bembis1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bembis1Application.class, args);
+	}
+
+}
---
+++ bembis2/src/test/java/com/example/bembis2/Bembis2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bembis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bembis2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
