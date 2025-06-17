a983cc7d8c732a95c6172d27ab0ff95a81bb3650
asilbek2403 <asilbeksafarov962@gmail.com>
2025-06-17 21:48:04 +0500

    Update Bembis3Application.java
    oddiy plus

--- bembis3/src/main/java/com/example/bembis3/Bembis3Application.java
+++ bembis3/src/main/java/com/example/bembis3/Bembis3Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+	
 public class Bembis3Application {
+	
 
-	public static void main(String[] args) {
+	public static void main(String[]
+				args) {
+		
 		SpringApplication.run(Bembis3Application.class, args);
-	}
-
+		
+		}
 }
